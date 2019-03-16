3b67dd193b0509b31542239ab644d37ee437766a
Wan Muhammad Azims <4zims95@gmail.com>
2019-03-16 21:27:38 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -19,13 +19,26 @@ self.addEventListener('install', function(event) {
   );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    cache.match(event.request).then(function(response) {
-      return response || fetch(event.request).then(function(response) {
-        cache.put(event.request, response.clone());
-        return response;
-      });
-    });
-  );
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.match(event.request).then(function (response) {
+                return response || fetch(event.request).then(function (response) {
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return fetch(event.request).then(function (response) {
+                cache.put(event.request, response.clone());
+                return response;
+            });
+        })
+    );
 });
