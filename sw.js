e6bc3e3161164fafff3f97073435b68e9ee39405
Wan Muhammad Azims <4zims95@gmail.com>
2019-03-16 21:32:42 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,21 +2,29 @@
 layout: null
 ---
 
-
 var urlsToCache = [];
 
-// Cache name: adjust version number to invalidate service worker cachce.
-var CACHE_NAME = 'james-ives-cache-v2';
+// Cache posts
+// Limits the number of posts that gets cached to 3
+// Reads a piece of front-matter in each post that directs the second loop to the folder where the assets are held
+{% for post in site.posts limit: site.paginate %}
+    urlsToCache.push("{{ post.url }}");
+    {% for file in site.static_files %}
+        {% if file.path contains post.assets %}
+            urlsToCache.push("{{ file.path }}");
+        {% endif %}
+    {% endfor %}
+{% endfor %}
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+var CACHE_NAME = 'jon-pitcherella-{{ site.time }}';
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
 self.addEventListener('fetch', function (event) {
